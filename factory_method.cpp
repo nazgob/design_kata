ab2992216e78234342b09a0e6d70fc10e25b01d3
nazgob <nazgob@gmail.com>
2010-11-07 14:39:00 +0100

    added factory method

---
+++ factory_method.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+
+class Product
+{
+  public:
+    virtual void Use() const = 0;
+};
+
+class ConcreteProduct : public Product 
+{
+  public:
+    virtual void Use() const
+    {
+      std::cout << "Using Concrete Product is really awesome!" << std::endl;
+    }
+};
+
+class Application
+{
+  public:
+    virtual Product* createProduct() const = 0;
+};
+
+class ConcreteApplication : public Application
+{
+  public:
+    virtual Product* createProduct() const
+    {
+      return new ConcreteProduct();
+    }
+}; 
+
+int main()
+{
+  std::cout << "Factory Method GoF Kata" << std::endl;
+
+  // we're programming to interfaces here; we know nothing about
+  // ConcreteProduct!
+  Application* application = new ConcreteApplication();
+  Product* product = application->createProduct();
+  product->Use();
+
+  delete application;
+  delete product;
+
+  return 0;
+}
+
